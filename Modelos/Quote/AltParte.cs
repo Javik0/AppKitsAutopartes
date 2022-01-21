928b64dc80d98a4e541c6bc74662baa52b3bd9ac
Javik0 <javico.davila@gmail.com>
2022-01-20 19:27:09 -0500

    Configuración uno a muchos

---
+++ Modelos/Quote/AltParte.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelos.Quote
+{
+    public class AltParte : IDBEntity
+    {
+        public int ParteId { get; set; }
+        public Parte Parte { get; set; }
+        public int ParteAlternaId { get; set; }
+        public Parte ParteAlterna { get; set; }
+
+    }
+}
---
+++ Modelos/Quote/Parte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelos.Quote
+{
+    public class Parte : IDBEntity
+    {
+        public int ParteId { get; set; }
+        public string Nombre { get; set; }
+        public int Stock { get; set; }
+        public List<AltParte> altPartes { get; set; }
+    }
+}
